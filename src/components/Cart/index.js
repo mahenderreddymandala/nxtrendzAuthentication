6f607cfcc3164f8e651d84dabbed85217a101514
mahenderreddymandala <mahenderreddy8790@gmail.com>
2022-12-06 15:16:09 +0000

    commit message

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+
+import Header from '../Header'
+
+import './index.css'
+
+const Cart = () => (
+  <div className="p-container">
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="product"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <div className="p-container">
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png "
+      alt="products"
+      className="products_list"
+    />
+  </div>
+)
+
+export default Products
